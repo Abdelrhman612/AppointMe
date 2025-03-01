784df2d3ff99af4d3d046f8fc8836162a389707d
Abdelrhman <abdo612200320@gmail.com>
2025-03-01 14:37:23 +0200

    add middleware is route register and login

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
-import { UserRouter } from "./routes/router";
-import { AppointmintsRouter } from "./routes/Appointmints";
+import { UserRouter } from "./routes/UsersRoute";
+import { AppointmintsRouter } from "./routes/AppointmintsRoute";
+import { AuthRouter } from "./routes/AutheUserRoute";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ connectDB();
 app.use(express.json());
 app.use("/api/users", UserRouter);
 app.use("/api/Appointmints", AppointmintsRouter);
+app.use("/api/users", AuthRouter);
 app.listen(port, () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
